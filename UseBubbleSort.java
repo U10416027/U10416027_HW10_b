be935e9ae9b198585b0089ddcbf54269e3457883
U10416027 <U10416027@go.utaipei.edu.tw>
2016-04-09 18:43:35 +0800

    Update UseBubbleSort.java

--- UseBubbleSort.java
+++ UseBubbleSort.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 public class UseBubbleSort{
 	public static void main(String[] args){
 		Scanner input = new Scanner(System.in);
+		System.out.println("How many integers you want to enter ?");
+		int x = input.nextInt();
 		
 	}
 }
