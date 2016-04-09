c1ea46a7dd180009cdbfa8dc7a9fcb9829ad5c4f
U10416027 <U10416027@go.utaipei.edu.tw>
2016-04-09 19:16:44 +0800

    Update BubbleSort.java

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,11 +1,23 @@
+import java.util.*;
+
 public class BubbleSort{
 	public void SortArray(int[] sortarray, int b){
 		int largest;
 		boolean check = true;
 		
+		while(check == true){
+			check = false;
+			
+			for(int i = 0; i < sortarray.length - 1; i++){
+				if(sortarray[i] < sortarray[i+1]){
+					largest = sortarray[i+1];
+					sortarray[i+1] = sortarray[i];
+					sortarray[i] = largest;
+					check = true;
+				}
+			}
+		}
 		
-		
-	
 		System.out.println("The Bubble Sort is");
 		for(int i = 0; i < b; i++){
 			System.out.println(sortarray[i]);
